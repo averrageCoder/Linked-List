3e7e8dae33927b9cbb3a4666e0e5b6268d707837
Harsh R Jain <rjainharsh@gmail.com>
2021-08-31 16:55:34 +0530

    ADDED: to create a linked list that appends at end

--- src/com/bridgelabz/linkedlist/LinkedListTest.java
+++ src/com/bridgelabz/linkedlist/LinkedListTest.java
@@ -4,14 +4,14 @@ public class LinkedListTest {
 
 	public static void main(String[] args) {
 		
-		MyNode<Integer> firstNode = new MyNode<Integer>(70);
+		MyNode<Integer> firstNode = new MyNode<Integer>(56);
 		MyNode<Integer> secondNode = new MyNode<Integer>(30);
-		MyNode<Integer> thirdNode = new MyNode<Integer>(56);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
 		
 		MyLinkedList myLinkedList = new MyLinkedList();
-		myLinkedList.add(firstNode);
-		myLinkedList.add(secondNode);
-		myLinkedList.add(thirdNode);
+		myLinkedList.append(firstNode);
+		myLinkedList.append(secondNode);
+		myLinkedList.append(thirdNode);
 		
 		myLinkedList.printMyNodes();
 		
