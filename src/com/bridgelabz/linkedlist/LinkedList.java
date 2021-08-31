10d46919c693dc7511b29901b969ea22001837d5
Harsh R Jain <rjainharsh@gmail.com>
2021-08-31 16:04:46 +0530

    ADDED: linkedlist class to create linked list

---
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.linkedlist;
+
+public class LinkedList {
+
+	public static void main(String[] args) {
+		
+		MyNode<Integer> firstNode = new MyNode<Integer>(56);
+		MyNode<Integer> secondNode = new MyNode<Integer>(30);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
+		
+		firstNode.setNext(secondNode);
+		secondNode.setNext(thirdNode);
+	}
+	
+}
