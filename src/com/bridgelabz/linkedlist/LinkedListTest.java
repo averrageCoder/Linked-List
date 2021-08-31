8e095d6adc051abf0c2006c25c3ec90188e9a4b9
Harsh R Jain <rjainharsh@gmail.com>
2021-08-31 17:06:29 +0530

    ADDED: to create a linked list that inserts node in between

--- src/com/bridgelabz/linkedlist/LinkedListTest.java
+++ src/com/bridgelabz/linkedlist/LinkedListTest.java
@@ -5,13 +5,13 @@ public class LinkedListTest {
 	public static void main(String[] args) {
 		
 		MyNode<Integer> firstNode = new MyNode<Integer>(56);
-		MyNode<Integer> secondNode = new MyNode<Integer>(30);
-		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
+		MyNode<Integer> secondNode = new MyNode<Integer>(70);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(30);
 		
 		MyLinkedList myLinkedList = new MyLinkedList();
-		myLinkedList.append(firstNode);
+		myLinkedList.add(firstNode);
 		myLinkedList.append(secondNode);
-		myLinkedList.append(thirdNode);
+		myLinkedList.insert(firstNode,thirdNode);
 		
 		myLinkedList.printMyNodes();
 		
