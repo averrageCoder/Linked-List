7019c424a9b7999849561c64343128aaf8e53aa0
Harsh R Jain <rjainharsh@gmail.com>
2021-08-31 18:08:29 +0530

    ADDED: ability to search and insert a node to linked list

--- src/com/bridgelabz/linkedlist/LinkedListTest.java
+++ src/com/bridgelabz/linkedlist/LinkedListTest.java
@@ -7,22 +7,17 @@ public class LinkedListTest {
 		MyNode<Integer> firstNode = new MyNode<Integer>(56);
 		MyNode<Integer> secondNode = new MyNode<Integer>(70);
 		MyNode<Integer> thirdNode = new MyNode<Integer>(30);
-		
+		MyNode<Integer> fourthNode = new MyNode<Integer>(40);
 		MyLinkedList myLinkedList = new MyLinkedList();
 		myLinkedList.add(firstNode);
 		myLinkedList.append(secondNode);
 		myLinkedList.insert(firstNode,thirdNode);
-		System.out.print("Before Popping : ");
-		myLinkedList.printMyNodes();
-		System.out.println("Popped element : "+myLinkedList.popLast().getKey());
-		System.out.print("After Popping : ");
+		System.out.print("Before Inserting : ");
 		myLinkedList.printMyNodes();
 		
-		if(myLinkedList.search(30)) {
-			System.out.println("Match found!");
-		}
-		else
-			System.out.println("Match not found!");
+		myLinkedList.searchAndInsert(30, fourthNode);
+		System.out.print("After Inserting : ");
+		myLinkedList.printMyNodes();
 	}
 	
 }
